3a863d2ba49a31c9b512b660c6e552e0f3f5141a
mcheng <micheng@linkedin.com>
2020-02-18 22:15:53 -0800

    Build nav links dynamically

--- js/app.js
+++ js/app.js
@@ -1,40 +1,58 @@
 /**
- * 
+ *
  * Manipulating the DOM exercise.
  * Exercise programmatically builds navigation,
  * scrolls to anchors from navigation,
  * and highlights section in viewport upon scrolling.
- * 
+ *
  * Dependencies: None
- * 
+ *
  * JS Version: ES2015/ES6
- * 
+ *
  * JS Standard: ESlint
- * 
+ *
 */
 
 /**
  * Define Global Variables
- * 
+ *
 */
 
 
 /**
  * End Global Variables
  * Start Helper Functions
- * 
+ *
 */
 
+function buildNav() {
+  var sectionNodes = document.querySelectorAll('[data-nav^="Section"]');
+
+  if (sectionNodes.length > 0) {
+    const documentFragment = new DocumentFragment();
+    for (const sectionNode of sectionNodes) {
+      const li = document.createElement('li');
+      const a = document.createElement('a');
+      a.textContent = sectionNode.getAttribute('data-nav');
+      a.setAttribute('href', `#${sectionNode.id}`)
+      a.classList.add('menu__link');
+      li.appendChild(a);
+      documentFragment.appendChild(li);
+    }
+    const ul = document.querySelector('#navbar__list');
+    ul.appendChild(documentFragment);
+  }
+};
 
 
 /**
  * End Helper Functions
  * Begin Main Functions
- * 
+ *
 */
 
 // build the nav
-
+buildNav();
 
 // Add class 'active' to section when near top of viewport
 
@@ -45,10 +63,10 @@
 /**
  * End Main Functions
  * Begin Events
- * 
+ *
 */
 
-// Build menu 
+// Build menu
 
 // Scroll to section on link click
 
